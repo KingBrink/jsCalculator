e9d99bf900e567f4ac09d639a8d62986923f7079
KingBrink <kyle.brink1602@gmail.com>
2024-05-17 15:00:29 +0200

    final

--- script/code.js
+++ script/code.js
@@ -1,30 +1,25 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+document.addEventListener("DOMContentLoaded", function() {
+    var inputBox = document.getElementById("inputBox");
+    var buttons = document.querySelectorAll("button");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click' , (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-
-        else if(e.target.innerHTML == 'AC'){
-            string = ""
-            input.value = string;
-        }
-
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-
-        else{
-            string += e.target.innerHTML;
-        input.value = string;
-        }
-
-       
-    })
-})
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var buttonText = this.textContent;
+            if (buttonText === "AC") {
+                inputBox.value = "";
+            } else if (buttonText === "DEL") {
+                inputBox.value = inputBox.value.slice(0, -1);
+            } else if (buttonText === "=") {
+                try {
+                    inputBox.value = eval(inputBox.value);
+                } catch (error) {
+                    inputBox.value = "Error";
+                }
+            } else if (buttonText === "%") {
+                inputBox.value = eval(inputBox.value) / 100;
+            } else {
+                inputBox.value += buttonText;
+            }
+        });
+    });
+});
